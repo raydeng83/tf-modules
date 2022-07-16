22648f2c9d6cc04a8ec9cececcff257f30b87727
raydeng83 <ray.deng83@gmail.com>
2022-07-16 09:24:37 -0400

    latest

--- services/webserver-cluster/variables.tf
+++ services/webserver-cluster/variables.tf
@@ -3,16 +3,34 @@
 # You must provide a value for each of these parameters.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "db_remote_state_bucket" {
-  description = "The name of the S3 bucket used for the database's remote state storage"
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
   type        = string
-  default     = "tf-ldeng-ch4-bucket-1"
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket for the database's remote state"
+  type        = string
 }
 
 variable "db_remote_state_key" {
-  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  description = "The path for the database's remote state in S3"
   type        = string
-  default     = "state/terraform.tfstate"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
+}
+
+variable "max_size" {
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -23,43 +41,5 @@ variable "db_remote_state_key" {
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
-  default     = 80
-}
-
-variable "alb_name" {
-  description = "The name of the ALB"
-  type        = string
-  default     = "terraform-asg-example"
-}
-
-variable "instance_security_group_name" {
-  description = "The name of the security group for the EC2 Instances"
-  type        = string
-  default     = "terraform-example-instance"
-}
-
-variable "alb_security_group_name" {
-  description = "The name of the security group for the ALB"
-  type        = string
-  default     = "terraform-example-alb"
-}
-
-variable "cluster_name" {
-  description = "The name to use for all the cluster resources"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "The type of EC2 Instance to run"
-  type        = string
-}
-
-variable "min_size" {
-  description = "The minimum number of EC2 Instances in the ASG"
-  type        = string
-}
-
-variable "max_size" {
-  description = "The maximum number of EC2 Instances in the ASG"
-  type        = number
+  default     = 8080
 }
